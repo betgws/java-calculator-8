4fa5e99dc4d83bbb4a037747778dee8673a3c0a8
betgws <dayeongyun184@gmail.com>
2025-10-17 13:25:32 +0900

    feat(StringCalculator): return 0 when input is empty

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,5 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    public static int add(String input) {
+
+        // input 값이 없을 경우 0
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+
 
 }
