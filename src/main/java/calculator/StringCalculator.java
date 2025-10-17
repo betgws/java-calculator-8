e27c5726d5960be6223678a02594950d4d1df333
betgws <dayeongyun184@gmail.com>
2025-10-17 13:32:29 +0900

    feat(StringCalculator): support custom delimiter pattern

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,14 +4,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+
     public static int add(String input) {
 
         // input 값이 없을 경우 0
         if (input == null || input.isEmpty()) {
             return 0;
         }
+
+        String delimiter = ",|:";
+        String numbers = input;
+
+        // 커스텀 구분자 형식: //;\n1;2;3
+        if (input.startsWith("//")) {
+            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+            if (m.matches()) {
+                delimiter += "|" + m.group(1);
+                numbers = m.group(2);
+            }
+        }
+
         return 0;
     }
-
-
 }
